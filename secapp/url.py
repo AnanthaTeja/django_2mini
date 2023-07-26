06d6bb97122abd2ae54ab51a76eda5976e9fc9a1
Anantha Teja Dasari <111896520+AnanthaTeja@users.noreply.github.com>
2023-07-27 02:04:22 +0530

    Add files via upload

--- secapp/url.py
+++ secapp/url.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from secapp import views
+from django.contrib.auth import views as g
 
 urlpatterns = [
     path("", views.home, name="hm"),
     path("abt/", views.about, name="ab"),
     path("cnt/", views.contact, name="ct"),
     path("reg/", views.register, name="rg"),
+    path("login/", g.LoginView.as_view(template_name="ht/login.html"), name="lg"),
+    path("logout/", g.LogoutView.as_view(template_name="ht/logout.html"), name="lgout"),
+    path("mail/", views.mailsending, name="ml"),
 ]
--- secapp/views.py
+++ secapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from .form import UserForm
+from secproject import settings
+from django.core.mail import send_mail
+from django.contrib import messages
 
 
 # Create your views here.
@@ -23,3 +26,22 @@ def register(request):
             return redirect("/")
     r = UserForm()
     return render(request, "ht/register.html", {"d": r})
+
+
+def mailsending(self):
+    if self.method == "POST":
+        sndr = self.POST["sn"]
+        sbj = self.POST["sb"]
+        m = self.POST["msg"]
+        t = settings.EMAIL_HOST_USER
+        # print(sndr, sbj, m, t)
+        b = send_mail(sbj, m, t, [sndr])
+        if b == 1:
+            messages.success(self, "Mail has been sent Successfully")
+            return redirect("/mail")
+
+        else:
+            messages.error(self, "Mail has not been sent")
+            return redirect("/mail")
+
+    return render(self, "ht/mail.html")
